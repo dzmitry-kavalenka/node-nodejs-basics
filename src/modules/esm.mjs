61786ae9ee4cc0228fdc2c8a3cdf5216b2903128
Dzmitry Kavalenka <dzmitriy.kavalenka@gmail.com>
2022-12-03 20:33:22 +0100

    feat: implement esm module

---
+++ src/modules/esm.mjs
@@ -0,0 +1,40 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import { readFile } from 'fs/promises';
+import "./files/c.js";
+
+const random = Math.random();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let unknownObject;
+
+unknownObject = JSON.parse(
+  await readFile(
+    new URL(`./files/${random > 0.5 ? "a" : "b"}.json`, import.meta.url)
+  )
+);
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export { unknownObject, myServer };
