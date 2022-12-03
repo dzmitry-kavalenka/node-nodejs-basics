a5025f58318a03297030a5addaac69b13f982ed4
Dzmitry Kavalenka <dzmitriy.kavalenka@gmail.com>
2022-12-03 22:03:44 +0100

    feat: implement streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "stream";
+import { stdin, stdout } from "node:process";
+
+const reverseTr = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().split("").reverse().join(""));
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  stdin.pipe(reverseTr).pipe(stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+import { stdin } from "node:process";
+
+const fileToWrite = "./src/streams/files/fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+  const ws = fs.createWriteStream(fileToWrite);
+  stdin.pipe(ws);
 };
 
 await write();
